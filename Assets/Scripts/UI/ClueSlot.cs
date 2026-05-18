32ab00df291d8ff69722d08f915837489bd46b33
이주현 <juhyun040100@gmail.com>
2026-05-18 18:43:15 +0900

    [feat] ChoiceUI & ClueUI 구성

---
+++ Assets/Scripts/UI/ClueSlot.cs
@@ -0,0 +1,10 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClueSlot : MonoBehaviour
+{
+    [SerializeField] private Image Image_Clue;
+    [SerializeField] private TextMeshProUGUI Text_Name;
+    [SerializeField] private TextMeshProUGUI Text_Description;
+}
