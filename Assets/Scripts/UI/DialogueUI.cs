67366fb14c7d4a7feb3780355f660daa7dda7cd7
이주현 <juhyun040100@gmail.com>
2026-05-18 15:49:58 +0900

    [feat] 기본 Dialogue 콘텐츠 구현

--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -18,6 +18,7 @@ public class DialogueUI : MonoBehaviour
     [SerializeField] private Image Image_Background;
     [SerializeField] private Image Image_Character;
     [SerializeField] private Image Image_NextArrow;
+    [SerializeField] private Image Image_Speaker;
 
     private string _currentID;
 
@@ -33,14 +34,61 @@ public class DialogueUI : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-
+            MoveToNextDialogue(_currentID);
         }
     }
 
     private void ShowDialogue(string id)
     {
+        var data = GameDataManager.Inst.GetDialogueData(id);
+        string speaker = data.Speaker;
+        string content = data.Content;
 
+        if (speaker != string.Empty)
+        {
+            Image_Speaker.gameObject.SetActive(true);
+            Text_Speaker.text = speaker;
+        }
+        else
+        {
+            Image_Speaker.gameObject.SetActive(false);
+        }
+
+        Text_Dialogue.text = content;
+
+        ChangeSpeakerCharacter(_currentID);
+        ChangeBackgroundImage(_currentID);
+    }
+
+    private void MoveToNextDialogue(string id)
+    {
+        string nextID = GameDataManager.Inst.GetDialogueData(id).NextID;
+        _currentID = nextID;
+
+        ShowDialogue(_currentID);
+    }
+
+    private void ChangeBackgroundImage(string id)
+    {
+        string background = GameDataManager.Inst.GetDialogueData(id).Background;
+
+        Image_Background.sprite = Resources.Load<Sprite>($"Background/{background}");
+    }
+
+    private void ChangeSpeakerCharacter(string id)
+    {
+        string characterFacial = GameDataManager.Inst.GetDialogueData(id).Facial;
+
+        if (characterFacial == string.Empty)
+        {
+            Image_Character.gameObject.SetActive(false);
+        }
+        else
+        {
+            Image_Character.gameObject.SetActive(true);
+            Image_Character.sprite = Resources.Load<Sprite>($"Character/{characterFacial}");
+        }
     }
 }
